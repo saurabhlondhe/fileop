4b99a1c42e0ca47c1cfa5a7325c4bd42198cd133
saurabhlondhe <saurabhlondhe1111@gmail.com>
2018-10-21 00:49:30 +0530

    modified

--- setup.py
+++ setup.py
@@ -5,19 +5,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="fileop",
-    version="0.0.1",
+    version="0.0.3",
     author="Saurabh Londhe",
     author_email="saurabhlondhe1111@gmail.com",
     description="All file operations without writing code for it",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/saurabhlondhe/python-Scripts/blob/master/sau.py",
+    url="https://github.com/saurabhlondhe/fileop",
     packages=['os'],
     classifiers=(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Linux",
     ),
 )
